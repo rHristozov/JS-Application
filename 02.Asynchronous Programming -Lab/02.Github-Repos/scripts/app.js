76a6147ca0fa0dcb58dec011f9b1158c40e21d73
Roman Hristozov <roman.hristozov@gmail.com>
2025-02-25 10:03:05 +0200

    Lab exercises

--- 02.Asynchronous Programming -Lab/02.Github-Repos/scripts/app.js
+++ 02.Asynchronous Programming -Lab/02.Github-Repos/scripts/app.js
@@ -1,3 +1,24 @@
 function loadRepos() {
-	console.log("TODO...");
+	const reposUlEl = document.getElementById('repos');
+	const nameEl = document.getElementById('username');
+	const username = nameEl.value;
+
+	const url = `https://api.github.com/users/${username}/repos`;
+
+	fetch(url)
+		.then((response) => response.json())
+		.then((reposData) => {
+			reposUlEl.innerHTML = '';
+	
+			for (const repo of reposData) {
+				const repoLi = document.createElement('li');
+				const repoLink = document.createElement('a');
+				repoLink.href = repo.url;
+				repoLink.textContent = repo.name;
+				repoLi.appendChild(repoLink)
+				reposUlEl.appendChild(repoLi)
+			}
+		})
+		.catch((error) => console.error(error))
+	
 }
--- 02.Asynchronous Programming -Lab/03.Gighub-Commits/app.js
+++ 02.Asynchronous Programming -Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,30 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const usernameEl = document.getElementById('username');
+    const repoNameEl = document.getElementById('repo');
+    const commitsEl = document.getElementById('commits');
+
+    const username = usernameEl.value;
+    const repoName = repoNameEl.value;
+    const url = `https://api.github.com/repos/${username}/${repoName}/commits`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            commitsEl.innerHTML = '';
+            for (const element of data) {
+                const message = element.commit.message;
+                const name = element.commit.author.name;
+                
+                const listEl = document.createElement('li');
+                listEl.textContent = `${name}: ${message}`
+                commitsEl.appendChild(listEl)
+
+            }
+        })
+        .catch(err => {
+            commitsEl.innerHTML = '';
+            const listEl = document.createElement('li');
+            listEl.textContent = `Error: ${err.status} (Not Found)`;
+            commitsEl.appendChild(listEl)
+        })
+    
 }
