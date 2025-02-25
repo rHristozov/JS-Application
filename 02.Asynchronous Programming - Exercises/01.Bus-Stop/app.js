ada594ab2c8732a9045e8f0cf3d36cb3201edf4f
Roman Hristozov <roman.hristozov@gmail.com>
2025-02-25 12:25:14 +0200

    1 exercise

--- 02.Asynchronous Programming - Exercises/01.Bus-Stop/app.js
+++ 02.Asynchronous Programming - Exercises/01.Bus-Stop/app.js
@@ -1,3 +1,27 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const busInput = document.getElementById('stopId');
+    const stopNameEl = document.getElementById('stopName');
+    const busOuput = document.getElementById('buses');
+
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${busInput.value}`
+
+    try {
+    const response = await fetch(url);
+    const data = await response.json();
+    stopNameEl.textContent = data.name;
+    // console.log(data);
+    busOuput.textContent = '';
+    const buses = Object.entries(data.buses)
+    
+    for (const bus of buses) {
+    const [busId, time] = bus;
+    const busLi = document.createElement('li');
+    busLi.textContent = `Bus ${busId} arrives in ${time} minutes`;
+    busOuput.appendChild(busLi);
+    }
+} catch (err) {
+    busOuput.textContent = '';
+    stopNameEl.textContent = 'Error';
+
+}
 }
